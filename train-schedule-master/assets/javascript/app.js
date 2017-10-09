1daec4ad81db2bdf17b2bf18bdfd369a4b3678ef
aaronebrown73 <aaronebrown73@gmail.com>
2017-10-09 11:03:38 -0700

    Add files via upload

---
+++ train-schedule-master/assets/javascript/app.js
@@ -0,0 +1,124 @@
+////////////////////////////////////////////////////////
+////////////////////  TRAIN        ////////////////////
+///////////////////      SCHEDULE ////////////////////
+/////////////////////////////////////////////////////
+
+
+
+// Global Variables
+var trainName = "";
+var trainDestination = "";
+var trainTime = "";
+var trainFrequency = "";
+var nextArrival = "";
+var minutesAway = "";
+
+// jQuery global variables
+var elTrain = $("#train-name");
+var elTrainDestination = $("#train-destination");
+// form validation for Time using jQuery Mask plugin
+var elTrainTime = $("#train-time").mask("00:00");
+var elTimeFreq = $("#time-freq").mask("00");
+
+
+// Initialize Firebase
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyALSxAuW21QEi33EKHh3YkZnPJcnuZ170E",
+    authDomain: "recent-user-project.firebaseapp.com",
+    databaseURL: "https://recent-user-project.firebaseio.com",
+    projectId: "recent-user-project",
+    storageBucket: "recent-user-project.appspot.com",
+    messagingSenderId: "45143378467"
+  };
+  firebase.initializeApp(config);
+
+// Assign the reference to the database to a variable named 'database'
+var database = firebase.database();
+
+database.ref("/trains").on("child_added", function(snapshot) {
+
+    //  create local variables to store the data from firebase
+    var trainDiff = 0;
+    var trainRemainder = 0;
+    var minutesTillArrival = "";
+    var nextTrainTime = "";
+    var frequency = snapshot.val().frequency;
+
+    // compute the difference in time from 'now' and the first train using UNIX timestamp, store in var and convert to minutes
+    trainDiff = moment().diff(moment.unix(snapshot.val().time), "minutes");
+
+    var trainRemainder = trainDiff % frequency;
+
+    var minutesTillArrival = frequency - trainRemainder;
+
+    var nextTrainTime = moment().add(minutesTillArrival, "m").format("hh:mm A");
+
+    $("#table-data").append(
+        "<tr><td>" + snapshot.val().name + "</td>" +
+        "<td>" + snapshot.val().destination + "</td>" +
+        "<td>" + frequency + "</td>" +
+        "<td>" + minutesTillArrival + "</td>" +
+        "<td>" + nextTrainTime + "  " + "<a><span class='glyphicon glyphicon-remove icon-hidden' aria-hidden='true'></span></a>" + "</td></tr>"
+    );
+
+    $("span").hide();
+
+
+    $("#table-data").on("click", "tr span", function() {
+        console.log(this);
+        var trainRef = database.ref("/trains/");
+        console.log(trainRef);
+    });
+});
+
+var storeInputs = function(event) {
+    event.preventDefault();
+
+    var trainName = elTrain.val().trim();
+    var trainDestination = elTrainDestination.val().trim();
+    var trainTime = moment(elTrainTime.val().trim(), "HH:mm").subtract(1, "years").format("X");
+    var trainFrequency = elTimeFreq.val().trim();
+
+    // add to firebase databse
+    database.ref("/trains").push({
+        name: trainName,
+        destination: trainDestination,
+        time: trainTime,
+        frequency: trainFrequency,
+        nextArrival: nextArrival,
+        minutesAway: minutesAway,
+        date_added: firebase.database.ServerValue.TIMESTAMP
+    });
+
+
+    //  empty form once submitted
+    elTrain.val("");
+    elTrainDestination.val("");
+    elTrainTime.val("");
+    elTimeFreq.val("");
+};
+
+// Calls storeInputs function if submit button clicked
+$("#btn-add").on("click", function(event) {
+    // form validation - if empty - alert
+    if (elTrain.val().length === 0 || elTrainDestination.val().length === 0 || elTrainTime.val().length === 0 || elTimeFreq === 0) {
+        alert("Please Fill All Required Fields");
+    } else {
+        // if form is filled out, run function
+        storeInputs(event);
+    }
+});
+
+// Calls storeInputs function if enter key is clicked
+$('form').on("keypress", function(event) {
+    if (event.which === 13) {
+        // form validation - if empty - alert
+        if (elTrain.val().length === 0 || elTrainDestination.val().length === 0 || elTrainTime.val().length === 0 || elTimeFreq === 0) {
+            alert("Please Fill All Required Fields");
+        } else {
+            // if form is filled out, run function
+            storeInputs(event);
+        }
+    }
+});
